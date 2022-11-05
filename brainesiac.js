42441eb804de2f3f544683ea8f4570a940ccd5d9
Bobby Raines <rainesrm@gmail.com>
2022-11-05 19:51:42 -0400

    Add login, change page links, add resume.

--- brainesiac.js
+++ brainesiac.js
@@ -6,3 +6,25 @@ function nav() {
       x.style.display = "block";
     }
   }
+
+
+function auth (username,password) {  
+  const loginForm = document.getElementById("login");
+  const loginButton = document.getElementById("log");
+  const loginErrorMsg = document.getElementById("message");
+  // When the login button is clicked, the following code is executed
+  //loginButton.addEventListener("click", (e) => {
+      // Prevent the default submission of the form
+     // e.preventDefault();
+      // Get the values input by the user in the form fields
+      //const username = loginForm.Uname.value;
+      //const password = loginForm.Pass.value;
+  
+      if (username === "brainesiac" && password === "brainesiac1234") {
+          location.assign("./loggedin.html");
+      } else {
+          // Otherwise, make the login error message show (change its oppacity)
+          loginErrorMsg.textContent  = "Login Failed" ;
+      }
+  })
+)
